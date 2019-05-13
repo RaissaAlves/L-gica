9fa955552e2bca5ca5a7a3ceb0af22ea4243aaf7
RaissaAlves <50590810+RaissaAlves@users.noreply.github.com>
2019-05-13 14:21:54 -0300

    Versão: completa

--- programação.1info2/dadoss.py
+++ programação.1info2/dadoss.py
@@ -1,6 +1,6 @@
 from random import randint #importa a função de gerar números aleatórios
 
-print("Olá 1info2")
+print("Jogo de dados")
 
 dado1 = randint(1, 6)  # Gera um número entre 1 e 6 aleatórios
 print("Dado 1:", dado1)
@@ -8,8 +8,22 @@ print("Dado 1:", dado1)
 dado2 = randint(1, 6)
 print("Dado 2:", dado2)
 
-dado3 = randint(1, 6)
+dado3 = randint(1, 6) #nome da variável não pode ter espaço
 print("Dado 3:", dado3)
 
 dado4 = randint(1, 6)
 print("Dado 4:", dado4)
+
+jogador1 = dado1 + dado2 
+jogador2 = dado3 + dado4
+
+print('Jogador 1: ', jogador1)
+print('Jogador 2: ', jogador2)
+
+if jogador1 > jogador2:
+    print("jogador 1 venceu!!!")
+else:
+    if jogador2 > jogador1:
+        print("jogador 2 venceu!!!")
+    else:
+        print("Empate!!!")
